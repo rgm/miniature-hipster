276e56f43d4350c029fda1e0953c0faaec87f80a
Ryan McCuaig <rgm@ryanmccuaig.net>
2013-02-25 15:46:08 -0800

    Initial commit

---
+++ pdftiler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pdftiler
+//
+//  Created by Ryan McCuaig on 2013/02/25.
+//  Copyright (c) 2013 Ryan McCuaig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
