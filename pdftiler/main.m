9fa5ea0d219915f14ec1f495e959a017d8c114fd
Ryan McCuaig <rgm@ryanmccuaig.net>
2013-02-26 09:33:43 -0800

    Add quick/dirty options parsing

--- pdftiler/main.m
+++ pdftiler/main.m
@@ -9,6 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <Lagrangian/Lagrangian.h>
 
+#include <getopt.h>
+
 @l3_suite("main");
 @l3_test("dummy test should fail") {
   l3_assert(YES, l3_equals(NO));
@@ -16,12 +18,47 @@
 
 int main(int argc, const char * argv[])
 {
+  long int rows = 1, cols = 1, dpi = 72;
+  char *end, *filename = "";
+  int c;
+  while (1) {
+    static struct option long_options[] = {
+      {"rows", required_argument, 0, 'x'},
+      {"cols", required_argument, 0, 'y'},
+      {"dpi", required_argument, 0, 'd'},
+      {0, 0, 0, 0}
+    };
+    int option_index = 0;
+    c = getopt_long(argc, (char *const *)argv, "x:y:", long_options, &option_index);
+    if (c == -1) { break; }
 
-  @autoreleasepool {
-      
-      NSLog(@"Hello, World!");
-      
+    switch (c) {
+
+      case 'x':
+        rows = strtol(optarg, &end, 10);
+        break;
+
+      case 'y':
+        cols = strtol(optarg, &end, 10);
+        break;
+
+      case 'd':
+        dpi = strtol(optarg, &end, 10);
+        break;
+
+      default:
+        abort();
+    }
   }
-    return 0;
+  if (optind < argc) {
+    filename = (char *)argv[optind++];
+  }
+  
+  @autoreleasepool {
+    
+    NSLog(@"dpi %ld rows %ld cols %ld filename %s", dpi, rows, cols, filename);
+    
+  }
+  return 0;
 }
 
